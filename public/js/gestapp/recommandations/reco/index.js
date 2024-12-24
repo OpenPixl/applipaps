9edf99fcf4e46a851fe0e80175e819fa48c6703e
Corwin40 <xavier.burke@openpixl.fr>
2024-12-24 09:50:24 +0100

    Correction Twig

--- public/js/gestapp/recommandations/reco/index.js
+++ public/js/gestapp/recommandations/reco/index.js
@@ -1,7 +1,1 @@
 import { showToast } from '../../../component/function.js';
-
-const test_notification = document.getElementById('test_notification');
-
-test_notification.addEventListener('click', function(event){
-    showToast('Bienvenue sur le site !', 'info');
-});
