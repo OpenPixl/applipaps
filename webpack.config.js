6157faf3190916128903988e061d61e562712e64
Corwin40 <xavier.burke@openpixl.fr>
2025-01-15 15:00:13 +0100

    DEV | Adaptation WebPackEncore pour PROD

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,12 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .copyFiles({
+        from: './assets/pwa',
+        to: 'pwa/[path][name].[ext]',
+        pattern: /\.(json|js)$/
+    })
 ;
+
 
 module.exports = Encore.getWebpackConfig();
